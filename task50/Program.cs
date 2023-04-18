ccf8a39a028c62bb362211946eb3e2fbf3a4333e
Zhanna <zhannass@gmai.com>
2023-04-18 18:39:18 +0200

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+//в двумерном массиве, и возвращает значение этого элемента или же 
+//указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//[17] -> такого числа в массиве нет 
+
+
+Console.WriteLine("Enter the number of row ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the number of columns ");
+int m = int.Parse(Console.ReadLine()!);
+int [,] numbers = new int [5,5];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("This number doesn't exist!");
+}
+else
+{
+    Console.WriteLine($"The number of {n} row and {m} column is - {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(0,10);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
